ab48572324c4041dfeb6ea3fd286446819642e02
BryanLizana <bryandelao5@gmail.com>
2018-10-05 15:50:19 -0500

    Update mongo-node simple

---
+++ MongoNode/controllers/products.controller.js
@@ -0,0 +1,15 @@
+exports.product_update = function (req, res) {
+    db.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+
+exports.list = function (req, res) {
+    db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+    })
+};
--- MongoNode/server.js
+++ MongoNode/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
+const router = express.Router();
+const product_controller = require(__dirname + '/controllers/products.controller');
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -16,25 +18,23 @@ app.get('/', (req, res) => {
   })
 
 
-app.get('/list', function(req, res) {
-    // res.send('List')
-    res.sendFile(__dirname + '/pages/index.html')
-})
+app.get('/list',product_controller.list)
 
 app.post('/quotes', (req, res) => {
     db.collection('quotes').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
-
         console.log('saved to database')
         res.redirect('/')
     })
 })
 
+router.put('/:id/update', product_controller.product_update);
+
 // MongoClient.connect('mongodb://blizana:delao123456@ds123603.mlab.com:23603/test_one', { useNewUrlParser: true } , (err, database) => {
 MongoClient.connect('mongodb://localhost:27017/test_one', { useNewUrlParser: true } , (err, database) => {
     if (err) return console.log(err)
     db = database.db('test_one') // whatever your database name is
-    app.listen(3030, () => {
-        console.log('listening on 3030')
+    app.listen(80, () => {
+        console.log('listening on 80')
     })
 })
